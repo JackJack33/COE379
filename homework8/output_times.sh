bd6a2815c8149bff62c769f958329ea3f5562e9b
JackJack33 <jackjack33mc@gmail.com>
2024-03-20 19:19:55 -0500

    initial hw8 files

---
+++ homework8/output_times.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+output_file="times.txt"
+
+> "$output_file"
+echo "threads size iterations precision time" > "$output_file"
+
+for (( threads = 1; threads <= 16; threads++ )); do
+    for (( size = 2; size <= 100000; size*=2 )); do
+
+	output=$(OMP_NUM_THREADS=$threads ./jacobi $size)
+	iterations=$(echo "$output" | grep "Converged" | awk '{print $3}')
+	precision=$(echo "$output" | grep "Converged" | awk '{print $6}')
+	time=$(echo "$output" | grep "Converged" | awk '{print $10}')
+	
+	echo "$threads $size $iterations $precision $time" >> "$output_file"
+	
+    done
+done
