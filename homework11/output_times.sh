5ed5d26babcce8dc25dfa58d23ae6d86f856dd7c
JackJack33 <jackjack33mc@gmail.com>
2024-04-10 12:23:51 -0500

    hw11 initial upload

---
+++ homework11/output_times.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+output_file="times.txt"
+
+> "$output_file"
+echo "vectorsize sequential_time parallel_time" > "$output_file"
+
+for (( vectorsize = 1; vectorsize <= 100000; vectorsize+=100 )); do
+    
+    output=$(OMP_NUM_THREADS=1 ./vectorsum $vectorsize)
+    sequential_time=$(echo "$output" | grep "Sequential" | awk '{print $3}')
+    parallel_time=$(echo "$output" | grep "Threads" | awk '{print $4}')
+    
+    echo "$vectorsize $sequential_time $parallel_time" >> "$output_file"
+    
+done
