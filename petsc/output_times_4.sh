8b0e030f6b3d0b0db0b150a154ca3c431583af66
JackJack33 <jackjack33mc@gmail.com>
2024-04-24 22:41:34 -0500

    final times

--- petsc/output_times_4.sh
+++ petsc/output_times_4.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-output_file="output_data.txt"
+output_file="times.txt"
 
 > "$output_file"
-echo "ksp_type pc_type sub_pc_type PCSetUp Flops_per_sec" > "$output_file"
+echo "ksp_type pc_type sub_pc_type PCSetUp Flops_per_sec Iterations MatMult" > "$output_file"
 
 for ksp_type in cg bcgs gmres; do
     for pc_type in bjacobi asm; do
         for sub_pc_type in lu ilu; do
             output=$(./ksp -n 100 -ksp_type $ksp_type -pc_type $pc_type -sub_pc_type $sub_pc_type -log_summary)
-            pc_setup=$(echo "$output" | grep "PCSetUp" | awk '{print $4}')
-            flops_per_sec=$(echo "$output" | grep "Flops/sec:" | awk '{print $4}')
-            echo "$ksp_type $pc_type $sub_pc_type $pc_setup $flops_per_sec" >> "$output_file"
+            pc_setup=$(echo "$output" | grep "PCSetUp" | awk 'NR==1 {print $4}')
+            flops_per_sec=$(echo "$output" | grep "Flops/sec:" | awk 'NR==1 {print $4}')
+            iterations=$(echo "$output" | grep "iterations" | awk 'NR==1 {print $6}')
+            mat_mult=$(echo "$output" | grep "MatMult" | awk 'NR==1 {print $2}')
+            echo "$ksp_type $pc_type $sub_pc_type $pc_setup $flops_per_sec $iterations $mat_mult" >> "$output_file"
         done
     done
 done
-
-
-
 
 
 
