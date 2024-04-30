b41deb00f204fb3b55d44b615c1e982364898b34
JackJack33 <jackjack33mc@gmail.com>
2024-04-30 09:58:57 -0500

    Update visualize.py

--- final/visualize.py
+++ final/visualize.py
@@ -1,9 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from PIL import Image
 
 colors = []
-with open("output.txt", "r") as file:
+with open("build/output.txt", "r") as file:
     lines = file.readlines()
+
+print("Read file, rendering...")
 
 for line in lines:
     color_str = line.split(": ")[1].strip()
@@ -14,13 +16,12 @@ lastline = lines[-1]
 width = int(lastline.split("[")[1].split("]")[0]) + 1
 height = int(lastline.split("[")[2].split("]")[0]) + 1
 
-matplotlib_colors = np.zeros((height, width, 3), dtype=np.uint8)
+ordered_colors = np.zeros((height, width, 3), dtype=np.uint8)
 for i, color in enumerate(colors):
     x = i % width
     y = i // width
-    matplotlib_colors[y][x] = color
+    ordered_colors[y][x] = color
 
-plt.figure(figsize=(width, height))
-plt.imshow(matplotlib_colors, origin='lower')
-plt.axis('off')
-plt.show()
+image = Image.fromarray(ordered_colors, "RGB")
+image.save("output.png")
+print("Done. Exported to output.png")
