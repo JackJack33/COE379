5b16130701047fd39707c5a5edaa96feb2de4ecc
JackJack33 <jackjack33mc@gmail.com>
2024-04-30 14:41:27 -0500

    mt 6

--- final/main.cxx
+++ final/main.cxx
@@ -30,7 +30,7 @@ int main() {
 			  [](float x, float y, float z) {return sphereSDF(x, y, z, 15, 3, 0, 3); });
   SceneObject blueSphere(SceneObjectType::OPAQUE, blue,
 			 [](float x, float y, float z) { return sphereSDF(x, y, z, 8, 1, 1, 2.5); });
-  SceneObject mirrorPlane(SceneObjectType::MIRROR, red,
+  SceneObject mirrorPlane(SceneObjectType::OPAQUE, red,
 			   [](float x, float y, float z) { return planeSDF(x, y, z, -4); });
 
   std::vector<SceneObject> objects = {greenSphere, mirrorPlane};
