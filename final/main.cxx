73d965752e1d2971ae0d2a3bfe2165b9e8e47e7f
JackJack33 <jackjack33mc@gmail.com>
2024-04-30 10:38:54 -0500

    fog fix

--- final/main.cxx
+++ final/main.cxx
@@ -29,7 +29,7 @@ int main() {
   std::vector<SceneObject> objects = {redSphere};
 
   Scene scene(objects, black);
-  Camera camera(0, 0, 0, 0, 0, M_PI / 2.0, 256, 256, scene, 0.00001, 300);
+  Camera camera(0, 0, 0, 0, 0, M_PI / 2.0, 256, 256, scene, 0.00001, 50);
 
   camera.InitializeRays();
   camera.March(5);
