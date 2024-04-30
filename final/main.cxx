186bd71ebc12efa0a62e83ee9c6379c0117a3ca8
JackJack33 <jackjack33mc@gmail.com>
2024-04-30 18:10:50 -0500

    swarm test

--- final/main.cxx
+++ final/main.cxx
@@ -11,11 +11,19 @@ float sphereSDF(float x_in, float y_in, float z_in,
   float dy = (y - y_in);
   float dz = (z - z_in);
   return std::sqrt( dx*dx + dy*dy + dz*dz) - r;
-}
+};
 
+float swarmSDF(float x_in, float y_in, float z_in,
+	       float x, float y, float z, float r, float spacing) {
+  float dx = std::fmod(x - x_in, spacing) - spacing / 2.0f;
+  float dy = std::fmod(y - y_in, spacing) - spacing / 2.0f;
+  float dz = std::fmod(z - z_in, spacing) - spacing / 2.0f;
+  return std::sqrt(dx*dx + dy*dy + dz*dz) - r;
+};
+  
 float planeSDF(float x, float y, float z, float h) {
   return y - h;
-}
+};
 
 int main() {
 
@@ -34,8 +42,11 @@ int main() {
 			   [](float x, float y, float z) { return planeSDF(x, y, z, -5); });
   SceneObject mirrorSphere(SceneObjectType::MIRROR, red,
   			   [](float x, float y, float z) { return sphereSDF(x, y, z, 8, -1, -1, 1); });
+  SceneObject redSwarm(SceneObjectType::OPAQUE, red,
+		       [](float x, float y, float z) { return swarmSDF(x, y, z, 0, 0, 0, 1, 5); });
 
-  std::vector<SceneObject> objects = {redSphere, blueSphere, greenSphere, mirrorSphere};
+  //  std::vector<SceneObject> objects = {redSphere, blueSphere, greenSphere, mirrorSphere};
+  std::vector<SceneObject> objects = {redSwarm};
   
   Scene scene(objects, black);
   Camera camera(0, 0, 0, 0, 0, M_PI / 2.0, 256, 256, scene, 0.001, 1000);
